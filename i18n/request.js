4787003e06ec6995ce2da97b29704a24ca700992
ghassan <ghassan.selman@infostrategic.com>
2025-08-09 23:11:32 +0300

    fix

--- i18n/request.js
+++ i18n/request.js
@@ -17,14 +17,14 @@
 // });
 
 import { getRequestConfig } from "next-intl/server";
-import { Locale, routing } from "./routing";
+import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   // This typically corresponds to the `[locale]` segment
   let locale = await requestLocale;
 
   // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale as Locale)) {
+  if (!locale || !routing.locales.includes(locale)) {
     locale = routing.defaultLocale;
   }
 
